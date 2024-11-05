ac4a9f9d1f79f633a8c43b084d66f3170e53fe78
me-js-bro <me.js.bro@gmail.com>
2024-11-05 20:33:55 +0600

    const variable ( read only )

---
+++ const variable/const.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// const variable ( read only )
+	const int WIDTH = 1920;
+	const int HEIGHT = 1080;
+	cout << "My screen resulation is " << WIDTH << 'x' << HEIGHT << 'p' << endl;
+	return 0;
+}
