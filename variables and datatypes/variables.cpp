d1103a3720eca4b9ba5efcf6f64853d4af405a86
me-js-bro <me.js.bro@gmail.com>
2024-11-05 17:05:17 +0600

    learned variables for double (floating) numbers

--- variables and datatypes/variables.cpp
+++ variables and datatypes/variables.cpp
@@ -2,16 +2,23 @@
 using namespace std;
 
 int main() {
+	// Int numbers.
 	int x, y, z, sum;
 	x = 5;
 	y = 7;
 	z = 9;
 	sum = x + y + z;
+	//cout << "The sum of x, y and z is " << sum << endl;
+	
+	// Floading numbers (With decimals)
+	double price1, price2, price3;
+	price1 = 10.4;
+	price2 = 11.798;
+	price3 = 9.1;
 
-	cout << y << endl;
-	cout << y << endl;
-	cout << z << endl;
-	cout << "The sum of x, y and z is " << sum << endl;
+	cout << "You have to pay: " << endl;
+	cout << price1 << " For apple, " << price2 << " For banana, " << price3 << " For mango." << endl;
+	
 
 	return 0;
 }
