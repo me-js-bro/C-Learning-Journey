ded6fdf8cd5e4c77bebfb888ce69e5cbc3e0b78e
me-js-bro <me.js.bro@gmail.com>
2024-11-05 21:02:41 +0600

    Arithmetics

---
+++ 4.arithmetics/arithmetic.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// addition
+	int students = 30;
+	//students ++;
+	//cout << students << endl;
+	
+
+	// subtraction
+	//students --;
+	//cout << students << endl;
+	
+
+	// multiplication
+	//students *= 2;
+	//cout << students << endl;
+	
+
+	// divission
+	//students /= 2;
+	//cout << students << endl;
+	
+
+	// modulus
+	int remainingStudents = students % 4;
+	cout << remainingStudents << endl;
+	return 0;
+}
