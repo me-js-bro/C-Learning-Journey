09bd54f9d906dca418421bb983a17ca63bc10633
me-js-bro <me.js.bro@gmail.com>
2024-11-05 16:54:39 +0600

    defined variables for int numbers and sub of them

---
+++ variables and datatypes/variables.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int x, y, z, sum;
+	x = 5;
+	y = 7;
+	z = 9;
+	sum = x + y + z;
+
+	cout << y << endl;
+	cout << y << endl;
+	cout << z << endl;
+	cout << "The sum of x, y and z is " << sum << endl;
+
+	return 0;
+}
