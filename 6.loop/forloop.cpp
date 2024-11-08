bb7eead09bbb8d38d0635aa6c1ddef30a8b4c09f
me-js-bro <me.js.bro@gmail.com>
2024-11-08 18:41:38 +0600

    for loop

---
+++ 6.loop/forloop.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string message = " Hey, I love you.";
+
+    for(int i = 1; i <= 1000; i ++) {
+       cout << i << "." << message << endl;
+    }
+    return 0;
+}
